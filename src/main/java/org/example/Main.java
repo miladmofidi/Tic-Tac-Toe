951a03fb21dc29569bd3cea12efda148aa9727f7
Milad Mofidi <miladm@amdocs.com>
2023-01-07 19:20:27 +0300

    Initial Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author milad mofidi
+ * user: ${USER} on ${DATE}
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world! This Tic-Tac-Toe Game");
+    }
+}
