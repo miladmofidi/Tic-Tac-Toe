50de2b019f2b953e79f288159ae50b74c00a3292
Milad Mofidi <miladm@amdocs.com>
2023-01-29 11:34:10 +0300

    Refining the structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static org.example.board.GameService.checkWinner;
 import static org.example.board.GameService.initBoard;
+import static org.example.board.GameService.printBoard;
 
 /**
  * @author milad mofidi
@@ -16,103 +18,6 @@ public class Main
     static String turn;
 
 
-    // CheckWinner method will
-    // decide the combination
-    // of three box given below.
-    static String checkWinner()
-    {
-        for (int a = 0; a < 8; a++)
-        {
-            String line = null;
-
-            switch (a)
-            {
-                case 0:
-                    line = board[0] + board[1] + board[2];
-                    break;
-                case 1:
-                    line = board[3] + board[4] + board[5];
-                    break;
-                case 2:
-                    line = board[6] + board[7] + board[8];
-                    break;
-                case 3:
-                    line = board[0] + board[3] + board[6];
-                    break;
-                case 4:
-                    line = board[1] + board[4] + board[7];
-                    break;
-                case 5:
-                    line = board[2] + board[5] + board[8];
-                    break;
-                case 6:
-                    line = board[0] + board[4] + board[8];
-                    break;
-                case 7:
-                    line = board[2] + board[4] + board[6];
-                    break;
-            }
-            //For X winner
-            if (line.equals("XXX"))
-            {
-                return "X";
-            }
-
-            // For O winner
-            else if (line.equals("OOO"))
-            {
-                return "O";
-            }
-        }
-
-        for (int a = 0; a < 9; a++)
-        {
-            if (Arrays.asList(board).contains(
-                    String.valueOf(a + 1)))
-            {
-                break;
-            }
-            else if (a == 8)
-            {
-                return "draw";
-            }
-        }
-
-        // To enter the X Or O at the exact place on board.
-        System.out.println(
-                turn + "'s turn; enter a slot number to place "
-                + turn + " in:");
-        return null;
-    }
-
-    // To print out the board.
-	/* |---|---|---|
-	| 1 | 2 | 3 |
-	|-----------|
-	| 4 | 5 | 6 |
-	|-----------|
-	| 7 | 8 | 9 |
-	|---|---|---|*/
-
-    static void printBoard()
-    {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                           + board[1] + " | " + board[2]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                           + board[4] + " | " + board[5]
-                           + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                           + board[7] + " | " + board[8]
-                           + " |");
-        System.out.println("|---|---|---|");
-    }
-
-
-
     public static void main(String[] args)
     {
         board= initBoard();
@@ -121,7 +26,7 @@ public class Main
         String winner = null;
 
         System.out.println("Welcome to 3x3 Tic Tac Toe.");
-        printBoard();
+        printBoard(board);
 
         System.out.println(
                 "X will play first. Enter a slot number to place X in:");
@@ -167,8 +72,8 @@ public class Main
                     turn = "X";
                 }
 
-                printBoard();
-                winner = checkWinner();
+                printBoard(board);
+                winner = checkWinner(board,turn);
             }
             else
             {
--- src/main/java/org/example/board/GameService.java
+++ src/main/java/org/example/board/GameService.java
@@ -1,5 +1,8 @@
 package org.example.board;
 
+
+import java.util.Arrays;
+
 /**
  * @author milad mofidi
  * user: miladm on 1/8/2023
@@ -16,5 +19,100 @@ public class GameService
             board[a] = String.valueOf(a + 1);
         }
         return board;
+    }
+
+    // To print out the board.
+	/* |---|---|---|
+	| 1 | 2 | 3 |
+	|-----------|
+	| 4 | 5 | 6 |
+	|-----------|
+	| 7 | 8 | 9 |
+	|---|---|---|*/
+
+    public static void printBoard(String[] board)
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
+    }
+
+    // CheckWinner method will
+    // decide the combination
+    // of three box given below.
+    public static String checkWinner(String[] board, String turn)
+    {
+        for (int a = 0; a < 8; a++)
+        {
+            String line = null;
+
+            switch (a)
+            {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+            //For X winner
+            if (line.equals("XXX"))
+            {
+                return "X";
+            }
+
+            // For O winner
+            else if (line.equals("OOO"))
+            {
+                return "O";
+            }
+        }
+
+        for (int a = 0; a < 9; a++)
+        {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1)))
+            {
+                break;
+            }
+            else if (a == 8)
+            {
+                return "draw";
+            }
+        }
+
+        // To enter the X Or O at the exact place on board.
+        System.out.println(
+                turn + "'s turn; enter a slot number to place "
+                + turn + " in:");
+        return null;
     }
 }
